17848be6f569d2676b4281778e3b27b8cc53c4c5
riyautekar25 <106662619+riyautekar25@users.noreply.github.com>
2026-01-11 20:15:48 +0530

    added basic details

---
+++ descriptive-statistics/mean_median_mode.py
@@ -0,0 +1,19 @@
+'''
+--> What is descriptive statistics?
+As the name suggest it describes features of your data such as centarl tendancy (mean, median, mode) and variability (range, standard deviation and variance).
+
+
+--> Purpose:
+1. Summarize large datasets
+2. Provide foundation for more complex statistical anaysis (inferential statistics, wil cover this later)
+
+TOPIC 1:
+CENTRAL TENDENCY
+A single center value of your data that represents or summarizes entire dataset.
+Mean: Average value of your data : When to use: Numerical data + roughly symmetrical data + NO extreme outliers --> NOTE** MEAN is affected by outliers
+Median: Middle value when data is sorted: When to use: Numerical data + outliers are present + Data is skewed + you want typical value --> Median is robust to outliers
+Mode: Most frequent value in your data: When to use: Categorical Data (can be used in Numerical case also) + you want most common cases
+
+'''
+
+
